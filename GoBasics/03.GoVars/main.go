a220b1d5ce12daaaafa136911ad7e2235f6bc747
Saksham (SK) Trivedi <trivedisaksham@gmail.com>
2024-10-06 19:28:11 +0530

    :Go Variables:

--- GoBasics/03.GoVars/main.go
+++ GoBasics/03.GoVars/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
+func main() {
 	fmt.Println("Learn Go Programming")
 
+	// Variable Init
+	var age int
+	age = 24 // Var Value
+
 	var name string = "SK"
-	
-	fmt.Println (name)
-	
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	// Boolean Value
+	FindOut := true
+
+	fmt.Println(FindOut)
+
 }
