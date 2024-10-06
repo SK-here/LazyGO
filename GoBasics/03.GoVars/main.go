4a1d98e319242d0855571dd0a1f6d16dc7f955c4
SK <trivedisaksham@gmail.com>
2024-10-06 20:13:51 +0530

    :Go Vars:

--- GoBasics/03.GoVars/main.go
+++ GoBasics/03.GoVars/main.go
@@ -5,20 +5,48 @@ import (
 )
 
 func main() {
+	
 	fmt.Println("Learn Go Programming")
 
-	// Variable Init
+	/*
+	Go is a statically-typed language, meaning the type of every variable is known at compile-time. Common variable types include:
+	Basic types: int, float64, bool, string
+	*/
+
+	// int
 	var age int
 	age = 24 // Var Value
-
-	var name string = "SK"
-
-	fmt.Println(name)
 	fmt.Println(age)
+
+	// String
+	var name string = "SK"
+	fmt.Println(name)
 
 	// Boolean Value
 	FindOut := true
-
 	fmt.Println(FindOut)
 
+	// Float
+	var pi float64 = 3.14159
+	fmt.Println(pi)
+
+	/*
+	2. Multiple Variable Declarations;
+	*/
+	var a, b, c int
+	var x, y = 1, 2
+	a = 4 
+	b = 6
+	c = 8
+
+	fmt.Println(x,y,a,b,c)
+
+	/*
+	Using short variable declaration 
+	*/
+
+	A := 42
+	Name := "John"
+
+	fmt.Println( A , Name)
 }
